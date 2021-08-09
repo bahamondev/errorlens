952c8600fe178c20b1b7ab32e3d59f40b303603f
GitHub Action <action@github.com>
2021-08-09 23:32:53 +0000

    Template cleanup

--- src/main/kotlin/com/github/bahamondev/errorlens/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/bahamondev/errorlens/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.bahamondev.errorlens.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.bahamondev.errorlens.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/bahamondev/errorlens/services/MyProjectService.kt
+++ src/main/kotlin/com/github/bahamondev/errorlens/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.bahamondev.errorlens.services
 
+import com.github.bahamondev.errorlens.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
