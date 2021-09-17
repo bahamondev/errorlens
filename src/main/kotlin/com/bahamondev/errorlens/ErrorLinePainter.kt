08872277363c32ae69ce3137350a0d4ee8d55b96
Jose Bahamonde Cordeiro <jose@bahamonde.dev>
2021-09-17 02:01:50 +0200

    style: split scopes in functions

--- src/main/kotlin/com/bahamondev/errorlens/ErrorLinePainter.kt
+++ src/main/kotlin/com/bahamondev/errorlens/ErrorLinePainter.kt
@@ -1,6 +1,6 @@
 package com.bahamondev.errorlens
 
-import com.intellij.openapi.command.undo.DocumentReferenceManager
+import com.intellij.openapi.editor.Document
 import com.intellij.openapi.editor.EditorLinePainter
 import com.intellij.openapi.editor.LineExtensionInfo
 import com.intellij.openapi.editor.markup.TextAttributes
@@ -9,33 +9,48 @@ import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.*
 import com.intellij.ui.SimpleTextAttributes
 import java.util.*
-import kotlin.collections.ArrayList
 
 
 internal class ErrorLinePainter : EditorLinePainter() {
+
     override fun getLineExtensions(
         project: Project,
         file: VirtualFile,
         lineNumber: Int
     ): MutableCollection<LineExtensionInfo>? {
-        val attributes = TextAttributes(
+        val psiFile = PsiManager.getInstance(project).findFile(file)
+        val document = psiFile?.let { PsiDocumentManager.getInstance(project).getDocument(it) }
+
+        if (psiFile == null || document == null) {
+            return null
+        }
+
+        val lineErrors = findLineErrors(psiFile, lineNumber, document)
+
+        return lineErrors.firstOrNull()?.let { Collections.singletonList(errorDescriptionToLineInfo(it)) }
+    }
+
+    private fun errorDescriptionToLineInfo(errorElement: PsiErrorElement) =
+        LineExtensionInfo(" ${errorElement.errorDescription}", textAttributes())
+
+    private fun textAttributes(): TextAttributes {
+        return TextAttributes(
             SimpleTextAttributes.REGULAR_ATTRIBUTES.fgColor,
             SimpleTextAttributes.REGULAR_ATTRIBUTES.bgColor,
             null,
             null,
             SimpleTextAttributes.REGULAR_ATTRIBUTES.fontStyle
         )
+    }
 
-        val psiFile = PsiManager.getInstance(project).findFile(file)
-        val document = psiFile?.let { PsiDocumentManager.getInstance(project).getDocument(it) }
-
-        if (psiFile == null) {
-            return Collections.singletonList(LineExtensionInfo(" ERROR NO FILE", attributes))
-        } else if (document == null) {
-            return Collections.singletonList(LineExtensionInfo(" ERROR NO DOCUMENT", attributes))
-        }
+    private fun findLineErrors(
+        psiFile: PsiFile,
+        lineNumber: Int,
+        document: Document
+    ): MutableList<PsiErrorElement> {
         val lineErrors = mutableListOf<PsiErrorElement>()
-        psiFile.accept(object : PsiRecursiveElementWalkingVisitor() {
+
+        psiFile.accept(object: PsiRecursiveElementWalkingVisitor() {
             override fun elementFinished(element: PsiElement?) {
                 if (element is PsiErrorElement) {
                     if (lineNumber == document.getLineNumber(element.textOffset)) {
@@ -44,11 +59,7 @@ internal class ErrorLinePainter : EditorLinePainter() {
                 }
             }
         })
-
-        if (lineErrors.isEmpty()) {
-            return Collections.singletonList(LineExtensionInfo(" NO ERRORS", attributes))
-        }
-
-        return Collections.singletonList(LineExtensionInfo(lineErrors.first().errorDescription, attributes))
+        return lineErrors
     }
+
 }
